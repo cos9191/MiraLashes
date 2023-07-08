a9fd0efd06dbb7eaf15c1b1660f5cb9c9dc6355b
Konstantin Fedorov <cos9191@mail.ru>
2023-07-08 14:04:52 +0700

    changed swiper's buttons

--- navMenu.js
+++ navMenu.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('header');
 const navMenu = document.querySelector('.header__nav-list')
 const burgerWrapper = document.querySelector('.header__burger-wrapper')
 const burgerButton = burgerWrapper.querySelector('.header__burger')
@@ -18,6 +19,23 @@ const initNavMenu = () => {
     navMenu.classList.toggle(navMenuClosedState)
     burgerButton.classList.toggle(burgerButtonOpenedState)
   })
+
+// Event listener for clicks on the document
+  document.addEventListener('click', function(event) {
+    let targetElement = event.target;
+
+    // Check if the clicked element is within the menu or the menu burger button
+    let isMenuClick = header.contains(targetElement) || targetElement === burgerWrapper;
+
+    // Check if the clicked element is within header or link
+    let isLinkInHeader = targetElement.tagName === 'A' && header.contains(targetElement);
+
+    // If the clicked element is outside the menu or an <a> tag in the header, close the menu
+    if (!isMenuClick || isLinkInHeader) {
+      navMenu.classList.remove(navMenuClosedState)
+      burgerButton.classList.remove(burgerButtonOpenedState)
+    }
+  });
 }
 
 export { initNavMenu }
